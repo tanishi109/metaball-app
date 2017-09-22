84f82fd45efb4bc1fa64c0384dfd51c5786792d1
tanishi109 <hayakawa@goodpatch.com>
2017-09-22 17:23:01 +0900

    draft

--- src/windows/main/index.tsx
+++ src/windows/main/index.tsx
@@ -1,40 +1,23 @@
 import * as React from "react";
 import {render} from "react-dom";
 
-import {
-  remote,
-} from "electron";
-
-const main = remote.require("./main").FuseBox.main("default/mainProcesses/main.js");
-
 interface Props {
 }
 interface State {
-  body: string;
 }
 
 class Main extends React.Component<Props, State> {
   constructor(props: Props) {
     super(props);
 
-    this.state = {
-      body: "hello world",
-    };
   }
 
   render(): JSX.Element {
-    const {body} = this.state;
-
     return (
       <div>
         <h1>sample app</h1>
-        <p>{body}</p>
       </div>
     );
-  }
-
-  handleClick() {
-    alert(main.fn());
   }
 }
 

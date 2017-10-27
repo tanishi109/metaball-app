71c7c2fcb48d006d2a9215bf0092f0a9dd45fd13
tanishi109 <hayakawa@goodpatch.com>
2017-10-27 16:30:43 +0900

    release build is faster

---
+++ rust/wasm/target/wasm32-unknown-emscripten/release/wasm.d
@@ -0,0 +1,1 @@
+/Users/rent-mac-029tanishi/workspace/electron-metaball/rust/wasm/target/wasm32-unknown-emscripten/release/wasm.js: /Users/rent-mac-029tanishi/workspace/electron-metaball/rust/wasm/src/main.rs
